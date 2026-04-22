197c7a7c80cf6eb5617a56602550a9ba112f7640
padraiccoffey399 <padraiccoffey@gmail.com>
2026-04-22 12:11:23 +0100

    Userr

--- src/main/java/Model/User.java
+++ src/main/java/Model/User.java
@@ -2,4 +2,46 @@ package Model;
 
 public class User {
 
+    private String name;
+    private String email;
+    private int age;
+
+    public User(String name, String email, int age) {
+        this.name = name;
+        this.email = email;
+        this.age = age;
+    }
+
+    // Getters
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    // Setters
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Name: " + name +
+                ", Email: " + email +
+                ", Age: " + age;
+    }
 }
