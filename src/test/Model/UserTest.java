1476c47c7c425e3a10be7b4530d48be8288b264a
Christopher Bolger <20111314@setu.ie>
2026-04-22 12:30:37 +0100

    Commit

---
+++ src/test/Model/UserTest.java
@@ -0,0 +1,41 @@
+package Model;
+
+import static org.junit.jupiter.api.Assertions.*;
+class UserTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getName() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getEmail() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getAge() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setName() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setEmail() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setAge() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testToString() {
+    }
+}
