e51490392d619af4369e3354d8aa8fc2c3a62cf3
Christopher Bolger <20111314@setu.ie>
2026-04-22 12:20:28 +0100

    Commit

--- src/main/java/Main/Driver.java
+++ src/main/java/Main/Driver.java
@@ -1,4 +1,20 @@
 package Main;
 
 public class Driver {
+    public static void main(String[] args) {
+        new Driver();
+    }
+    public void Driver(){
+        startMenu();
+    }
+
+    private void startMenu(){
+        System.out.println("""
+                Welcome to SETU Sells!
+                ----------------------
+                Nothing here yet!
+                ----------------------
+                """);
+    }
+
 }
---
+++ src/main/test/Model/UserTest.java
@@ -0,0 +1,14 @@
+package Model;
+
+import org.junit.jupiter.api.Assertions.*;
+
+class UserTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+    }
+}
